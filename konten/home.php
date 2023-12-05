53fb90301484ad53b68b4e6ec40b53ab1b596fbf
DMS473 <daffamuis0407@gmail.com>
2023-12-06 05:37:16 +0700

    push ke 2

---
+++ konten/home.php
@@ -0,0 +1,56 @@
+<!-- ini halaman home -->
+<form action="register.php" method="post">
+    <p>&nbsp;</p>
+    <div align="center" class="style1">
+        <!-- ini halaman home -->
+        <p>REGISTER</p>
+    </div>
+    <!-- cari Member
+    <from action="module/cari.php" method="get"> 
+    <input type="text" name="username" placeholder="ketikan username" />
+    <input type="submit" value="cari" />
+    </form><br /> -->
+
+    <table width="496" border="0" align="center">
+        <tr>
+            <td width="163">Nama depan :</td>
+            <td width="317"><input type="text" name="namadep" /></td>
+        </tr>
+        <tr>
+            <td>Nama Belakang :</td>
+            <td><input type="text" name="namabel" /></td>
+        </tr>
+        <tr>
+            <td>Username :</td>
+            <td><input type="text" name="username" /></td>
+        </tr>
+        <tr>
+            <td>Password :</td>
+            <td><input type="password" name="password" /></td>
+        </tr>
+        <tr>
+            <td>Usia :</td>
+            <td><input type="text" name="usia" /></td>
+        </tr>
+        <tr>
+            <td>Jenis Kelamin :</td>
+            <td><input type="text" name="jk" /></td>
+        </tr>
+        <tr>
+            <td>Tempat Tanggal Lahir :</td>
+            <td><input type="text" name="ttl" /></td>
+        </tr>
+        <tr>
+            <td>Email :</td>
+            <td><input type="text" name="email" /></td>
+        </tr>
+        <tr>
+            <td>Nomor Telepon :</td>
+            <td><input type="text" name="notel" /></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" value="input" /></td>
+        </tr>
+    </table>
+</form>
